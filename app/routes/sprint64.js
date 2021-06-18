b6791681ced94be1108d9cac2ff7c0111ae85476
Jonathan Inch <jonathan.inch@engineering.digital.dwp.gov.uk>
2021-06-18 12:20:26 +0100

    commercial address task iteration

--- app/routes/sprint64.js
+++ app/routes/sprint64.js
@@ -21,12 +21,42 @@ router.post('/sprint64/task-lists', (req, res) => {
 })
 ;
 
-
-  // Referral to get next
-  router.post('/sprint64/commercial-address', (req, res) => {
-    res.redirect('/sprint64/next-task')
-  })
+router.post('/sprint64/commercial-address', function(req, res) {
+  if ( req.body['approve-commercial'] === 'no-update' ) {
+    res.redirect('address');
+  } else {
+    res.redirect('next-task');
+  }
+  });
   ;
 
+
+  router.post('/sprint64/address', (req, res) => {
+    res.redirect('address-1')
+  });
+
+  router.post('/sprint64/address-1', (req, res) => {
+    res.redirect('move-date')
+  });
+
+  router.post('/sprint64/move-date', (req, res) => {
+    res.redirect('living-with')
+  });
+
+  router.post('/sprint64/living-with', (req, res) => {
+    res.redirect('homephone-address')
+  });
+
+  router.post('/sprint64/homephone-address', (req, res) => {
+    res.redirect('poa')
+  });
+
+  router.post('/sprint64/poa', (req, res) => {
+    res.redirect('declaration')
+  });
+
+  router.post('/sprint64/declaration', (req, res) => {
+    res.redirect('next-task')
+  });
 
   module.exports = router;
