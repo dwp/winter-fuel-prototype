b8805cc6512809869e2ab100eb3921cf3804265d
Al Robinson <77453029+Al-Robinson@users.noreply.github.com>
2021-07-07 17:47:58 +0100

    hack2work

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Winter Fuel Payment',
+  serviceName: 'GOV.UK',
 
   // Default port that prototype runs on
   port: '3000',
