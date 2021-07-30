6301e5af1b091712546abe1c23ee8a82f11133ad
Jonathan Inch <jonathan.inch@engineering.digital.dwp.gov.uk>
2021-07-30 10:58:22 +0100

    Death arrears task updates

--- app/routes/tasks-pay-death-arrears.js
+++ app/routes/tasks-pay-death-arrears.js
@@ -8,6 +8,103 @@ router.use((req, res, next) => {
   next()
 })
 
+// Address -------------------------------------------------------------------
+router.post('/tasks/pay-death-arrears/address', function(req, res) {
+  if (req.body['postcode'] === 'NE2 1YL') {
+    res.redirect('address-no-result');
+  } else if (req.body['postcode'] === "NE65 0AP") {
+    res.redirect('address-carehome');
+  } else if (req.body['postcode'] === "DH3 4AE") {
+    res.redirect('/sprint52c/address-carehome');
+  } else {
+    res.redirect('address-1');
+  }
+});
+
+// Address select, yes or search address
+router.post('/tasks/pay-death-arrears/address-1', function(req, res) {
+  if (req.body['address-change'] === '27 Kent Street, Amble, NE65 0LZ') {
+    res.redirect('move-date');
+  } else {
+    res.redirect('address-search');
+  }
+});
+
+
+// Care home address select, yes or search address
+router.post('/tasks/pay-death-arrears/address-carehome', function(req, res) {
+  if (req.body['address-change'] === 'Dolphin View Care Home, Harbour Road, Amble, NE65 0AP') {
+    res.redirect('carehome-move-date');
+  } else {
+    res.redirect('address-search');
+  }
+});
+
+// Address search
+router.post('/tasks/pay-death-arrears/address-search', function(req, res) {
+  if (req.body['address-search-postcode'] === 'NE2 1YL') {
+    res.redirect('address-search-no-result');
+  } else {
+    res.redirect('address-search-result');
+  }
+});
+
+// Address search select, yes or search address
+router.post('/tasks/pay-death-arrears/address-search-result', function(req, res) {
+  if (req.body['address-search-change'] === '27 Kent Street, Amble, NE65 0LZ') {
+    res.redirect('move-date');
+  } else {
+    res.redirect('TBC');
+  }
+});
+
+
+// Move date
+router.post('/tasks/pay-death-arrears/move-date', (req, res) => {
+  res.redirect('living-with')
+});
+
+// Living with anyone at address change
+router.post('/tasks/pay-death-arrears/living-with', function(req, res) {
+  if (req.body['living-with'] === 'Lives with someone else who gets Winter Fuel Payment') {
+    res.redirect('living-with-age');
+  } else {
+    res.redirect('living-with-date');
+  }
+});
+
+
+
+router.post('/tasks/pay-death-arrears/living-with-age', (req, res) => {
+  if (req.body['before-1954'] === 'Yes') {
+    res.redirect('living-with-over-80');
+  } else {
+    res.redirect('living-with-date');
+  }
+});
+
+router.post('/tasks/pay-death-arrears/living-with-over-80', (req, res) => {
+  res.redirect('living-with-date')
+});
+
+router.post('/tasks/pay-death-arrears/living-with-date', (req, res) => {
+  res.redirect('declaration')
+});
+
+router.post('/tasks/pay-death-arrears/declaration', (req, res) => {
+  res.redirect('death-arrears-task-1')
+});
+
+
+// Living with anyone at address change
+router.post('/tasks/pay-death-arrears/living-with-q-week', function(req, res) {
+  if (req.body['living-with-q-week'] === 'Lives with someone else who gets Winter Fuel Payment') {
+    res.redirect('living-with-age-q-week');
+  } else {
+    res.redirect('living-with-date');
+  }
+});
+
 // Task to - DAP details, next of kin details or return to queue
 
 router.post('/tasks/pay-death-arrears/death-arrears-task-1', function(req, res) {
