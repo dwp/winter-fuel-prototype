8712b1f1c93ae43072b4a9e7ceed523a665ae5de
Jonathan Inch <jonathan.inch@engineering.digital.dwp.gov.uk>
2021-09-20 17:06:40 +0100

    record view prototype restructure

--- app/routes/update-details.js
+++ app/routes/update-details.js
@@ -11,9 +11,9 @@ router.use((req, res, next) => {
 
 
 
-router.post('/update-details/update-details-options', function(req, res) {
-  if (req.session.data["update-details-options"] === "update-details") {
-    res.redirect('update-details');
+router.post('/record-view/overview-tab/update-details/update-details-options', function(req, res) {
+  if (req.session.data["update-details-options"] === "current-details") {
+    res.redirect('current-details');
   } else if (req.session.data["update-details-options"] === "dead") {
     res.redirect('date-of-death');
   }
@@ -25,7 +25,7 @@ router.post('/update-details/update-details-options', function(req, res) {
 }
 });
 
-router.post('/update-details/update-details-options-dlo', function(req, res) {
+router.post('/record-view/overview-tab/update-details/update-details-options-dlo', function(req, res) {
   if (req.session.data["update-details-options"] === "remove-dlo") {
   res.redirect('address');
 }
@@ -34,7 +34,7 @@ router.post('/update-details/update-details-options-dlo', function(req, res) {
 
 
 // Address -------------------------------------------------------------------
-router.post('/update-details/address', function(req, res) {
+router.post('/record-view/overview-tab/update-details/address', function(req, res) {
   if (req.body['postcode'] === 'NE2 1YL') {
     res.redirect('address-no-result');
   } else if (req.body['postcode'] === "NE65 0AP") {
@@ -42,40 +42,49 @@ router.post('/update-details/address', function(req, res) {
   } else if (req.body['postcode'] === "DH3 4AE") {
     res.redirect('/sprint52c/address-carehome');
   } else {
-    res.redirect('address-1');
+    res.redirect('address-search-results');
   }
 });
 
 // Address select, yes or search address
-router.post('/update-details/address-1', function(req, res) {
+router.post('/record-view/overview-tab/update-details/address-search-results', function(req, res) {
   if (req.body['address-change'] === '27 Kent Street, Amble, NE65 0LZ') {
     res.redirect('move-date');
   } else {
-    res.redirect('address-search');
+    res.redirect('address-manual-search');
+  }
+});
+
+// Address search
+router.post('/record-view/overview-tab/update-details/address-manual-search', function(req, res) {
+  if (req.body['address-search-postcode'] === 'NE2 1YL') {
+    res.redirect('address-search-no-result');
+  } else {
+    res.redirect('address-manual-search-result');
   }
 });
 
 
 // Care home address select, yes or search address
-router.post('/update-details/address-carehome', function(req, res) {
+router.post('/record-view/overview-tab/update-details/address-carehome', function(req, res) {
   if (req.body['address-change'] === 'Dolphin View Care Home, Harbour Road, Amble, NE65 0AP') {
     res.redirect('carehome-move-date');
   } else {
-    res.redirect('address-search');
+    res.redirect('address-manual-search');
   }
 });
 
 // Address search
-router.post('/update-details/address-search', function(req, res) {
+router.post('/record-view/overview-tab/update-details/address-search', function(req, res) {
   if (req.body['address-search-postcode'] === 'NE2 1YL') {
     res.redirect('address-search-no-result');
   } else {
-    res.redirect('address-search-result');
+    res.redirect('address-manual-search-result');
   }
 });
 
 // Address search select, yes or search address
-router.post('/update-details/address-search-result', function(req, res) {
+router.post('/record-view/overview-tab/update-details/address-manual-search-result', function(req, res) {
   if (req.body['address-search-change'] === '27 Kent Street, Amble, NE65 0LZ') {
     res.redirect('move-date');
   } else {
@@ -83,14 +92,18 @@ router.post('/update-details/address-search-result', function(req, res) {
   }
 });
 
+// Move date
+router.post('/record-view/overview-tab/update-details/overseas-address', (req, res) => {
+  res.redirect('move-date')
+});
 
 // Move date
-router.post('/update-details/move-date', (req, res) => {
+router.post('/record-view/overview-tab/update-details/move-date', (req, res) => {
   res.redirect('living-with')
 });
 
 // Living with anyone at address change
-router.post('/update-details/living-with', function(req, res) {
+router.post('/record-view/overview-tab/update-details/living-with', function(req, res) {
   if (req.body['living-with'] === 'Lives with someone else who gets Winter Fuel Payment') {
     res.redirect('living-with-age');
   } else {
@@ -100,7 +113,7 @@ router.post('/update-details/living-with', function(req, res) {
 
 
 
-router.post('/update-details/living-with-age', (req, res) => {
+router.post('/record-view/overview-tab/update-details/living-with-age', (req, res) => {
   if (req.body['before-1954'] === 'Yes') {
     res.redirect('living-with-over-80');
   } else {
@@ -108,17 +121,17 @@ router.post('/update-details/living-with-age', (req, res) => {
   }
 });
 
-router.post('/update-details/living-with-over-80', (req, res) => {
+router.post('/record-view/overview-tab/update-details/living-with-over-80', (req, res) => {
   res.redirect('living-with-date')
 });
 
-router.post('/update-details/living-with-date', (req, res) => {
+router.post('/record-view/overview-tab/update-details/living-with-date', (req, res) => {
   res.redirect('update-anything-else')
 });
 
 
 // Living with anyone at address change
-router.post('/update-details/living-with-q-week', function(req, res) {
+router.post('/record-view/overview-tab/update-details/living-with-q-week', function(req, res) {
   if (req.body['living-with-q-week'] === 'Lives with someone else who gets Winter Fuel Payment') {
     res.redirect('living-with-age-q-week');
   } else {
@@ -126,21 +139,21 @@ router.post('/update-details/living-with-q-week', function(req, res) {
   }
 });
 
-router.post('/update-details/homephone', (req, res) => {
+router.post('/record-view/overview-tab/update-details/homephone', (req, res) => {
   res.redirect('update-anything-else')
 });
 
 // Home phone number removal
-router.post('/update-details/homephone-remove', function(req, res) {
+router.post('/record-view/overview-tab/update-details/homephone-remove', function(req, res) {
   if (req.body['homephone-remove'] === 'Yes') {
     res.redirect('update-anything-else');
   } else {
-    res.redirect('homephone');
+    res.redirect('current-details');
   }
 });
 
 // Power of attourney
-router.post('/update-details/poa', function(req, res) {
+router.post('/record-view/overview-tab/update-details/poa', function(req, res) {
   if (req.body['poa'] === 'Yes') {
     res.redirect('poa-people');
   } else {
@@ -148,11 +161,11 @@ router.post('/update-details/poa', function(req, res) {
   }
 });
 
-router.post('/update-details/poa-people', (req, res) => {
+router.post('/record-view/overview-tab/update-details/poa-people', (req, res) => {
   res.redirect('update-anything-else')
 });
 
-router.post('/update-details/update-anything-else', (req, res) => {
+router.post('/record-view/overview-tab/update-details/update-anything-else', (req, res) => {
   if (req.body['update-anything-else'] === 'no') {
    res.redirect('declaration')
   } else if (req.body['update-details-options'] === 'address') {
@@ -168,7 +181,7 @@ router.post('/update-details/update-anything-else', (req, res) => {
 
 
 
-router.post('/update-details/declaration', function(req, res) {
+router.post('/record-view/overview-tab/update-details/declaration', function(req, res) {
   if (req.session.data['movemonth'] === "07") {
     res.redirect('over-payment');
   } else if (req.session.data['movemonth'] === "7") {
@@ -178,33 +191,47 @@ router.post('/update-details/declaration', function(req, res) {
   } else if (req.session.data['movemonth'] === "8") {
     res.redirect('make-payment');
   } else {
-    res.redirect('overview');
+    res.redirect('/record-view/overview-tab/overview');
   }
 });
 
 // Make payment to contact
-router.post('/update-details/make-payment', (req, res) => {
-  res.redirect('/update-details/overview-topup')
+router.post('/record-view/overview-tab/update-details/make-payment', (req, res) => {
+  res.redirect('/record-view/overview-tab/update-details/overview-topup')
 });
 
-router.post('/update-details/update-details-changed', (req, res) => {
+// Mobile phone number change back to contact
+router.post('/record-view/overview-tab/update-details/mobilephone', (req, res) => {
+  res.redirect('update-anything-else')
+});
+
+// Mobile phone number removal
+router.post('/record-view/overview-tab/update-details/mobilephone-remove', function(req, res) {
+  if (req.body['mobilephone-remove'] === 'yes') {
+    res.redirect('update-anything-else');
+  } else {
+    res.redirect('/record-view/overview-tab/update-details/current-details');
+  }
+});
+
+router.post('/record-view/overview-tab/update-details/update-details-changed', (req, res) => {
   res.redirect('overview')
 });
 
-router.post('/update-details/over-payment', (req, res) => {
+router.post('/record-view/overview-tab/update-details/over-payment', (req, res) => {
   res.redirect('overview')
 });
 
-router.post('/update-details/update-details', (req, res) => {
+router.post('/record-view/overview-tab/update-details/current-details', (req, res) => {
   res.redirect('overview')
 });
 
 
-router.post('/update-details/date-of-death', (req, res) => {
+router.post('/record-view/overview-tab/update-details/date-of-death', (req, res) => {
   res.redirect('overview')
 });
 
-router.post('/update-details/stop-payments-reason', (req, res) => {
+router.post('/record-view/overview-tab/update-details/stop-payments-reason', (req, res) => {
   res.redirect('overview')
 });
 
