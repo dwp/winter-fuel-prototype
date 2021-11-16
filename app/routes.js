7d303d626783e4518fd853fc4b28e02efaaa23b8
Al Robinson <77453029+Al-Robinson@users.noreply.github.com>
2021-11-16 11:47:28 +0000

    routing radio buttons

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const router = express.Router()
+const radioButtonRedirect = require('radio-button-redirect')
+router.use(radioButtonRedirect)
 
 // copy the const name and the file path to new version
 const eligibilitychecker = require('./routes/eligibility-checker');
