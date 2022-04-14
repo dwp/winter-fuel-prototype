f7a54795bd423d3fafa0a5df4ce30687171ec37a
Jonathan Inch <jonathan.inch@engineering.digital.dwp.gov.uk>
2022-04-14 16:49:39 +0100

    baseline eligibility checker - ready for iteration

--- app/routes/eligibility-checker.js
+++ app/routes/eligibility-checker.js
@@ -16,28 +16,33 @@ router.use((req, res, next) => {
 
 // Start screen
 
-router.post('/eligibility-checker/check-eligibility', (req, res) => {
-  res.redirect('/eligibility-checker/date-of-birth')
+router.post('/current/eligibility-checker/check-eligibility', (req, res) => {
+  res.redirect('/current/eligibility-checker/date-of-birth')
 })
 ;
 
   // Date of birth
 
-  router.post('/eligibility-checker/date-of-birth', (req, res) => {
-    res.redirect('/eligibility-checker/residency')
-  })
-  ;
+  
+
+  router.post('/current/eligibility-checker/date-of-birth', function(req, res) {
+    if ( req.body['dob-year'] > 1955) {
+      res.redirect('too-young');
+    } else {
+      res.redirect('residency');
+    }
+  });
 
   // Date of birth
 
-  router.post('/eligibility-checker/too-young', (req, res) => {
-    res.redirect('/eligibility-checker/check-eligibility')
+  router.post('/current/eligibility-checker/too-young', (req, res) => {
+    res.redirect('/current/eligibility-checker/check-eligibility')
   })
   ;
 
   // Claimed Winter Fuel Payment
 
-  router.post('/eligibility-checker/claimed', function(req, res) {
+  router.post('/current/eligibility-checker/claimed', function(req, res) {
     if ( req.body['claimed'] === 'yes' ) {
       res.redirect('residency-type');
     } else {
@@ -47,7 +52,7 @@ router.post('/eligibility-checker/check-eligibility', (req, res) => {
 
   // Living
 
-  router.post('/eligibility-checker/residency', function(req, res) {
+  router.post('/current/eligibility-checker/residency', function(req, res) {
     if ( req.body['living'] === 'united-kingdom' ) {
       res.redirect('receiving-pc');
     } else {
@@ -57,15 +62,15 @@ router.post('/eligibility-checker/check-eligibility', (req, res) => {
 
   // living overseas
 
-  router.post('/eligibility-checker/overseas', (req, res) => {
-    res.redirect('/eligibility-checker/check-eligibility')
+  router.post('/current/eligibility-checker/overseas', (req, res) => {
+    res.redirect('/current/eligibility-checker/check-eligibility')
   })
   ;
 
 
   // Benefits PC?
 
-  router.post('/eligibility-checker/receiving-pc', function(req, res) {
+  router.post('/current/eligibility-checker/receiving-pc', function(req, res) {
     if ( req.body['pension-credit'] === 'yes' ) {
       res.redirect('residency-type-pc');
     } else if ( req.body['pension-credit'] === 'no' ) {
@@ -75,14 +80,14 @@ router.post('/eligibility-checker/check-eligibility', (req, res) => {
 
   // Benefits SP?
 
-  router.post('/eligibility-checker/receiving-sp', (req, res) =>  {
+  router.post('/current/eligibility-checker/receiving-sp', (req, res) =>  {
       res.redirect('residency-type')
   });
 
 
 // Query
 
-router.post('/eligibility-checker/query', function(req, res) {
+router.post('/current/eligibility-checker/query', function(req, res) {
 if ( req.body['query'] === 'eligibility' ) {
   res.redirect('date-of-birth');
 } else {
@@ -94,16 +99,13 @@ if ( req.body['query'] === 'eligibility' ) {
 
   // Residency type SP
 
-  router.post('/eligibility-checker/residency-type', function(req, res) {
+  router.post('/current/eligibility-checker/residency-type', function(req, res) {
     if ( req.body['where-were-you-living'] === 'hospital' ) {
       res.redirect('hospital');
     } else if ( req.body['where-were-you-living'] === 'carehome' ) {
       res.redirect('care-home');
     } else if ( req.body['where-were-you-living'] === 'no-abode' ) {
-      if (req.session.data["dob-year"] === "1939"){
-        res.redirect('no-abode-over-80');
-       } else {
-        res.redirect('no-abode-under-80');}
+      res.redirect('no-abode-under-80');
     } else if ( req.body['where-were-you-living'] === 'prison' ) {
       res.redirect('prison');
     } else {
@@ -111,33 +113,26 @@ if ( req.body['query'] === 'eligibility' ) {
     }
   });
 
-
-
   // Residency type PC
 
-  router.post('/eligibility-checker/residency-type-pc', function(req, res) {
+  router.post('/current/eligibility-checker/residency-type-pc', function(req, res) {
     if ( req.body['where-were-you-living'] === 'hospital' ) {
       res.redirect('hospital-pc');
     } else if ( req.body['where-were-you-living'] === 'carehome' ) {
       res.redirect('care-home-pc');
     } else if ( req.body['where-were-you-living'] === 'no-abode' ) {
-      if (req.session.data["dob-year"] === "1939"){
-        res.redirect('no-abode-over-80');
-       } else {
-        res.redirect('no-abode-under-80');}
+      res.redirect('no-abode-under-80');
     } else if ( req.body['where-were-you-living'] === 'prison' ) {
       res.redirect('prison');
-    } else if (req.session.data["dob-year"] === "1939"){
-      res.redirect('over-80-pension-credit-payment');
-     } else {
-      res.redirect('under-80-pension-credit-payment');
-  }
+    } else {
+      res.redirect('pension-credit');
+    }
   });
 
 
   // Hospital
 
-  router.post('/eligibility-checker/hospital', function(req, res) {
+  router.post('/current/eligibility-checker/hospital', function(req, res) {
     if ( req.body['hospital-admission'] === 'yes' ) {
       res.redirect('who');
     } else {
@@ -147,7 +142,7 @@ if ( req.body['query'] === 'eligibility' ) {
 
   // Hospital PC
 
-  router.post('/eligibility-checker/hospital-pc', function(req, res) {
+  router.post('/current/eligibility-checker/hospital-pc', function(req, res) {
     if ( req.body['hospital-admission'] === 'yes' ) {
       res.redirect('pension-credit');
     } else {
@@ -157,12 +152,9 @@ if ( req.body['query'] === 'eligibility' ) {
 
     // Care or nursing home 13 weeks
 
-    router.post('/eligibility-checker/care-home', function(req, res) {
+    router.post('/current/eligibility-checker/care-home', function(req, res) {
       if ( req.body['care-home-admission'] === 'yes' ) {
-        if (req.session.data["dob-year"] === "1939") {
-         res.redirect('over-80-care-home-no-pc');
-        } else {
-         res.redirect('under-80-care-home-no-pc');}
+        res.redirect('care-home-full');
       } else {
         res.redirect('care-home-shared');
       }
@@ -171,12 +163,9 @@ if ( req.body['query'] === 'eligibility' ) {
 
     // Care or nursing home PC 13 weeks
 
-      router.post('/eligibility-checker/care-home-pc', function(req, res) {
+      router.post('/current/eligibility-checker/care-home-pc', function(req, res) {
         if ( req.body['care-home-admission'] === 'yes' ) {
-          if (req.session.data["dob-year"] === "1939") {
-            res.redirect('over-80-care-home-pc');
-           } else {
-            res.redirect('under-80-care-home-pc');}
+          res.redirect('shared-payment-pc');
         } else {
           res.redirect('care-home-over-pc');
         }
@@ -184,19 +173,25 @@ if ( req.body['query'] === 'eligibility' ) {
 
     // Who (Living with)
 
-      router.post('/eligibility-checker/who', function(req, res) {
+      router.post('/current/eligibility-checker/who', function(req, res) {
         if ( req.body['who-do-you-live-with'] === 'yes' ) {
           res.redirect('living-with');
-        } else if (req.session.data["dob-year"] === "1939"){
-          res.redirect('full-payment-over-80');
-         } else {
+        } else {
           res.redirect('full-payment-under-80');
-      }
+        }
+      });
+
+      router.post('/current/eligibility-checker/living-with-over-80', function(req, res) {
+        if ( req.body['live-with-age-over-80'] === 'yes' ) {
+          res.redirect('under-80-shared-with-over-80');
+        } else {
+          res.redirect('over-80-shared');
+        }
       });
 
       // Who (Living with)
 
-        router.post('/eligibility-checker/who-pc', function(req, res) {
+        router.post('/current/eligibility-checker/who-pc', function(req, res) {
           if ( req.body['who-do-you-live-with'] === 'yes' ) {
             res.redirect('shared-payment-pc');
           } else {
@@ -207,60 +202,41 @@ if ( req.body['query'] === 'eligibility' ) {
 
       // Living with (age)
 
-        router.post('/eligibility-checker/living-with', function(req, res) {
+        router.post('/current/eligibility-checker/living-with', function(req, res) {
           if ( req.body['live-with-age'] === 'yes' ) {
-            if (req.session.data["dob-year"] === "1939") {
-              res.redirect('living-with-over-80');
-             } else {
-              res.redirect('shared-payment');}
+            res.redirect('living-with-over-80');
           } else {
-            if (req.session.data["dob-year"] === "1939") {
-              res.redirect('full-payment-over-80');
-             } else {
-              res.redirect('full-payment-under-80');}
+            res.redirect('full-payment-under-80');
           }
         });
 
-      // Living with over 80
-
-      router.post('/eligibility-checker/living-with-over-80', function(req, res) {
-        if ( req.body['live-with-age-over-80'] === 'yes' ) {
-          if (req.session.data["dob-year"] === "1939") {
-            res.redirect('over-80-shared-over-80');
-           } else {
-            res.redirect('under-80-shared-with-over-80');}
-        } else {
-          res.redirect('over-80-shared');
-        }
-      });
-
       // Shared payment to overpayment find
 
-      router.post('/eligibility-checker/shared-payment', (req, res) => {
-        res.redirect('/eligibility-checker/find-1')
+      router.post('/current/eligibility-checker/shared-payment', (req, res) => {
+        res.redirect('/current/eligibility-checker/find-1')
       })
       ;
 
       // Shared payment to overpayment find
 
-      router.post('/eligibility-checker/find-1', (req, res) => {
-        res.redirect('/eligibility-checker/find-result-1')
+      router.post('/current/eligibility-checker/find-1', (req, res) => {
+        res.redirect('/current/eligibility-checker/find-result-1')
       })
       ;
 
       // Overpayment branch
 
-      router.post('/eligibility-checker/find-result-1', (req, res) => {
-        res.redirect('/eligibility-checker/security-1')
+      router.post('/current/eligibility-checker/find-result-1', (req, res) => {
+        res.redirect('/current/eligibility-checker/security-1')
       })
       ;
 
-      router.post('/eligibility-checker/security-1', (req, res) => {
-        res.redirect('/eligibility-checker/postcode')
+      router.post('/current/eligibility-checker/security-1', (req, res) => {
+        res.redirect('/current/eligibility-checker/postcode')
       })
       ;
 
-      router.post('/eligibility-checker/address-check-1', function(req, res) {
+      router.post('/current/eligibility-checker/address-check-1', function(req, res) {
         if ( req.body['address-match'] === 'yes' ) {
           res.redirect('poa-1');
         } else {
@@ -268,48 +244,48 @@ if ( req.body['query'] === 'eligibility' ) {
         }
       });
 
-      router.post('/eligibility-checker/poa-1', (req, res) => {
-        res.redirect('/eligibility-checker/check-1')
+      router.post('/current/eligibility-checker/poa-1', (req, res) => {
+        res.redirect('/current/eligibility-checker/check-1')
       })
       ;
 
-      router.post('/eligibility-checker/check-1', (req, res) => {
-        res.redirect('/eligibility-checker/declaration')
+      router.post('/current/eligibility-checker/check-1', (req, res) => {
+        res.redirect('/current/eligibility-checker/declaration')
       })
       ;
 
       // Full payment
 
-      router.post('/eligibility-checker/full-payment', (req, res) => {
-        res.redirect('/eligibility-checker/check-eligibility')
+      router.post('/current/eligibility-checker/full-payment', (req, res) => {
+        res.redirect('/current/eligibility-checker/check-eligibility')
       })
       ;
 
       // Find a person
 
-      router.post('/eligibility-checker/find', (req, res) => {
-        res.redirect('/eligibility-checker/find-result')
+      router.post('/current/eligibility-checker/find', (req, res) => {
+        res.redirect('/current/eligibility-checker/find-result')
       })
       ;
 
       // Found person to security
 
-      router.post('/eligibility-checker/find-result', (req, res) => {
-        res.redirect('/eligibility-checker/security')
+      router.post('/current/eligibility-checker/find-result', (req, res) => {
+        res.redirect('/current/eligibility-checker/security')
       })
       ;
 
       // Found person to security
 
-      router.post('/eligibility-checker/security', (req, res) => {
-        res.redirect('/eligibility-checker/postcode')
+      router.post('/current/eligibility-checker/security', (req, res) => {
+        res.redirect('/current/eligibility-checker/postcode')
       })
       ;
 
 
 // Who (Care home during Q week)
 
-router.post('/eligibility-checker/live-with-carehome', function(req, res) {
+router.post('/current/eligibility-checker/live-with-carehome', function(req, res) {
   if ( req.body['live-with-carehome'] === 'yes' ) {
     res.redirect('live-with-carehome-13weeks');
   } else {
@@ -319,7 +295,7 @@ router.post('/eligibility-checker/live-with-carehome', function(req, res) {
 
 // Care home during Q week over 13 weeks?
 
-router.post('/eligibility-checker/live-with-carehome-13weeks', function(req, res) {
+router.post('/current/eligibility-checker/live-with-carehome-13weeks', function(req, res) {
   if ( req.body['carehome-13weeks'] === 'yes' ) {
     res.redirect('shared-payment');
   } else {
@@ -329,7 +305,7 @@ router.post('/eligibility-checker/live-with-carehome-13weeks', function(req, res
 
 
 
-router.post('/eligibility-checker/live-with-anyone-else', function(req, res) {
+router.post('/current/eligibility-checker/live-with-anyone-else', function(req, res) {
   if ( req.body['carehome-other'] === 'yes' ) {
     res.redirect('shared-payment');
   } else {
@@ -339,71 +315,71 @@ router.post('/eligibility-checker/live-with-anyone-else', function(req, res) {
 
 // Power of attorney
 
-router.post('/eligibility-checker/poa', (req, res) => {
-  res.redirect('/eligibility-checker/check')
+router.post('/current/eligibility-checker/poa', (req, res) => {
+  res.redirect('/current/eligibility-checker/check')
 })
 ;
 
 // Postcode underpayment
 
-router.post('/eligibility-checker/postcode', (req, res) => {
-  res.redirect('/eligibility-checker/select-address')
+router.post('/current/eligibility-checker/postcode', (req, res) => {
+  res.redirect('/current/eligibility-checker/select-address')
 })
 ;
 
 // Postcode overpayment
 
-router.post('/eligibility-checker/postcode-1', (req, res) => {
-  res.redirect('/eligibility-checker/select-address-1')
+router.post('/current/eligibility-checker/postcode-1', (req, res) => {
+  res.redirect('/current/eligibility-checker/select-address-1')
 })
 ;
 
 // Check
 
-router.post('/eligibility-checker/postcode', (req, res) => {
-  res.redirect('/eligibility-checker/select-address')
+router.post('/current/eligibility-checker/postcode', (req, res) => {
+  res.redirect('/current/eligibility-checker/select-address')
 })
 ;
 
 // Select address to move date
 
-router.post('/eligibility-checker/select-address', (req, res) => {
-  res.redirect('/eligibility-checker/move-date')
+router.post('/current/eligibility-checker/select-address', (req, res) => {
+  res.redirect('/current/eligibility-checker/move-date')
 })
 ;
 
 // Select address to move date
 
-router.post('/eligibility-checker/select-address-1', (req, res) => {
-  res.redirect('/eligibility-checker/move-date-1')
+router.post('/current/eligibility-checker/select-address-1', (req, res) => {
+  res.redirect('/current/eligibility-checker/move-date-1')
 })
 ;
 
 // Move date to power of attorney
 
-router.post('/eligibility-checker/move-date', (req, res) => {
-  res.redirect('/eligibility-checker/poa')
+router.post('/current/eligibility-checker/move-date', (req, res) => {
+  res.redirect('/current/eligibility-checker/poa')
 })
 ;
 
 // Move date to power of attorney 2
 
-router.post('/eligibility-checker/move-date-1', (req, res) => {
-  res.redirect('/eligibility-checker/poa-1')
+router.post('/current/eligibility-checker/move-date-1', (req, res) => {
+  res.redirect('/current/eligibility-checker/poa-1')
 })
 ;
 
 
 // Occupants
 
-router.post('/eligibility-checker/occupants', (req, res) => {
-  res.redirect('/eligibility-checker/check')
+router.post('/current/eligibility-checker/occupants', (req, res) => {
+  res.redirect('/current/eligibility-checker/check')
 })
 ;
 
 // Check to declaration
 
-router.post('/eligibility-checker/check', function(req, res) {
+router.post('/current/eligibility-checker/check', function(req, res) {
   if ( req.body['change'] === 'yes' ) {
     res.redirect('declaration');
   } else {
@@ -414,8 +390,8 @@ router.post('/eligibility-checker/check', function(req, res) {
 
 // Declaration to confirmation
 
-router.post('/eligibility-checker/declaration', (req, res) => {
-  res.redirect('/eligibility-checker/confirmation')
+router.post('/current/eligibility-checker/declaration', (req, res) => {
+  res.redirect('/current/eligibility-checker/confirmation')
 })
 ;
 
